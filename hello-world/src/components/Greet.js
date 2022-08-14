ff0b623ad6d3916db6795601d74c39e7dc12721f
unknown <hubert780225@gmail.com>
2022-08-15 00:35:12 +0800

    feat/新增 增加Functional Component基本建構方法

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// function Greet() {
+//     return <h1>Hello Hubert</h1>
+// }
+
+// import { Greet } from "./components/Greet";
+// export const Greet = () => <h1>Hello Hubert</h1>
+
+const Greet = () => <h1>Hello Hubert</h1>
+
+export default Greet;
