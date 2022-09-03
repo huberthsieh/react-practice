fd0f940de3d74d5e93a70ce0b32282239585ce3c
unknown <hubert780225@gmail.com>
2022-09-04 00:52:23 +0800

    fix/修改 Print execution order of lifecycle methods in component mounted and updated phase

--- hello-world/src/components/LifecycleA.js
+++ hello-world/src/components/LifecycleA.js
@@ -21,11 +21,32 @@ class LifecycleA extends Component {
         console.log('LifecycleA componentDidMount')
     }
 
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('LifecycleA getSnapshotBeforeUpdate')
+        return null
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () => {
+        this.setState({
+            name: 'Codevolution'
+        })
+    }
+
     render() {
         console.log('LifecycleA render')
         return (
             <div>
                 <div>Lifecycle A</div>
+                <button onClick={this.changeState}>Change State!</button>
                 <LifecycleB />
             </div>
         );
