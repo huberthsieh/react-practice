0c53efcc39673907d6e1b398f61ecf8fa204c331
unknown <hubert780225@gmail.com>
2022-09-15 00:55:36 +0800

    feat/新增 Add the forward ref method

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 // import RefsDemo from "./components/RefsDemo";
-import FocusInput from "./components/FocusInput";
+// import FocusInput from "./components/FocusInput";
+import ForwardParentInput from "./components/ForwardParentInput";
 
 
 function App() {
     return (
         <div className="App">
+            <ForwardParentInput />
             {/*<RefsDemo />*/}
-            <FocusInput />
+            {/*<FocusInput />*/}
         </div>
     );
 }
