503e76de6390e809cb37bae8b8031b1fe0fc631f
unknown <hubert780225@gmail.com>
2023-04-14 05:29:25 +0800

    feat/新增 Context Base

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
-import ClickCounterTwo from "./components/ClickCounterTwo";
-import HoverCounterTwo from "./components/HoverCounterTwo";
-import User from "./components/User";
-import CommonCounter from "./components/CommonCounter";
+import ComponentC from "./components/ComponentC";
+import { UserProvider } from './components/userContext';
 
 
 function App() {
     return (
         <div className="App">
-            {/*<ClickCounterTwo />*/}
-            {/*<HoverCounterTwo />*/}
-            {/*<User render={(isLoggedIn) => isLoggedIn ? 'Hubert Hsieh' : 'Guest'} />*/}
-            <CommonCounter>
-                {(count, incrementCount) => (
-                    <ClickCounterTwo count={count} incrementCount={incrementCount}/>
-                )}
-            </CommonCounter>
-
-            <CommonCounter>
-                {(count, incrementCount) => (
-                    <HoverCounterTwo count={count} incrementCount={incrementCount}/>
-                )}
-            </CommonCounter>
+            <UserProvider value="Hubert">
+                <ComponentC />
+            </UserProvider>
         </div>
     );
 }
